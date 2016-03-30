f6dba3ec187c13b48d989e424b244041f1c9fa43
Cristian <cristidrincu@gmail.com>
2016-03-30 19:39:23 +0300

    Delete array-slice-method.js

--- array-slice-method.js
+++
@@ -1,8 +0,0 @@
-var synetoCompleteTeam = ["cristi", "robert", "csaba", "dan", "roberto", "vadim", "dragos", "olivia", "romeo", "dan dan"];
-
-//extract the marketing team
-var marketingTeam = synetoCompleteTeam.slice(synetoCompleteTeam.indexOf("vadim"), synetoCompleteTeam.indexOf("romeo"));
-
-console.log("Syneto complete team before slice: " + synetoCompleteTeam);
-console.log("Syneto complete team after slice: " + synetoCompleteTeam);
-console.log("Marketing team after slicing syneto complete team: " + marketingTeam);
